2562efa7ca0ac4615586a16021ef6b3b93c21728
Pavel Hrdý <pavel.hrdy@homecredit.eu>
2022-05-25 22:08:23 +0200

    update search bar design

--- client/src/components/CustomAppBar.js
+++ client/src/components/CustomAppBar.js
@@ -1,21 +1,32 @@
 import AppBar from '@mui/material/AppBar'
-import Typography from '@mui/material/Typography'
 import Toolbar from '@mui/material/Toolbar'
 import TextField from '@mui/material/TextField/TextField'
 import Box from '@mui/material/Box/Box'
-import IconButton from '@mui/material/IconButton/IconButton'
-import SearchIcon from "@mui/icons-material/Search";
+
 import { ReactComponent as LogoSvg } from '../images/logo.svg';
+import Autocomplete from '@mui/material/Autocomplete/Autocomplete'
 
 const CustomAppBar = () => <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: '#c1edff' }}>
     <Toolbar>
         <LogoSvg width={120} height={70} />
-        <Box marginLeft="auto"><TextField id="outlined-search" size='small' type="search" sx={{ input: { color: 'black', backgroundColor: 'white' } }} /></Box>
-        <IconButton type="submit" aria-label="search">
-            <SearchIcon style={{ fill: "white" }} />
-        </IconButton>
+        <Box marginLeft="auto" width={300}>
+            <Autocomplete
+                freeSolo
+                disableClearable
+                options={[]}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Vyhledávání"
+                        InputProps={{
+                            ...params.InputProps,
+                            type: 'search',
+                        }}
+                    />
+                )}
+            />
+        </Box>
     </Toolbar>
-
 </AppBar>
 
 export default CustomAppBar
