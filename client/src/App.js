66457df498a208dc58c75503e355d27563687fb1
Pavel Hrdý <pavel.hrdy@homecredit.eu>
2022-05-28 11:19:15 +0200

    delete chapter detail

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import Box from '@mui/material/Box/Box';
 import CssBaseline from '@mui/material/CssBaseline/CssBaseline';
 import Toolbar from '@mui/material/Toolbar/Toolbar';
-import ChapterDetail from 'pages/ChapterDetail';
 import CustomAppBar from 'components/CustomAppBar';
 import SubchapterDetail from 'pages/SubchapterDetail';
 import VideoDetail from 'pages/VideoDetail';
@@ -23,7 +22,6 @@ const App = () =>
           <Toolbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/chapter/:chapterId" element={<ChapterDetail />} />
             <Route path="/chapter/:chapterId/:subchapterId" element={<SubchapterDetail />} />
             <Route path="/chapter/:chapterId/:subchapterId/video/:videoId" element={<VideoDetail />} />
           </Routes>
--- client/src/pages/ChapterDetail.js
+++
@@ -1,7 +0,0 @@
-const { default: Box } = require("@mui/system/Box/Box");
-
-const ChapterDetail = () => <Box>
-
-</Box>
-
-export default ChapterDetail
